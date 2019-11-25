8b953883408579b83399cab9cced899b9352558b
florent <bonneau.florent44@gmail.com>
2019-11-25 12:50:51 +0000

    test

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
     <title>file_explorer</title>
 </head>
 <body>
-    
+        <?php
+
+            $fichier = fopen('essaie.txt','r+');
+
+
+            $i=1;
+            while(i<= 10){
+            $ligne = fgets($fichier);
+            echo $ligne;
+            $i++;
+            }
+            fclose($fichier);
+
+
+        ?>
 </body>
 </html>
